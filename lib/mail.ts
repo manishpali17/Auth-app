84d0235b1679f1aa29a4ff263af932f32846dfe4
manish-pali <129107899+manish-pali@users.noreply.github.com>
2024-02-27 00:41:23 +0530

    changed resend to nodemailer

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,12 +1,19 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
+const sendingEmail = process.env.EMAIL;
+
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS,
+  },
+});
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  await transport.sendMail({
+    from: sendingEmail,
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token}</p>`,
@@ -16,8 +23,8 @@ export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  await transport.sendMail({
+    from: sendingEmail,
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
@@ -27,15 +34,15 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
-try {
-    const res = await resend.emails.send({
-      from: "onboarding@resend.dev",
+  try {
+    const res = await transport.sendMail({
+      from: sendingEmail,
       to: email,
       subject: "Confirm your email",
       html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
     });
-    console.log(res)
-} catch (error) {
-  console.log(error)
-}
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
 };
